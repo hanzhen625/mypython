759f29facb5e7595ab22abc0afa2952ac849505b
aisnote <aisnote@gmail.com>
2017-08-02 21:13:59 -0700

    I'm the best

---
+++ Myproject.py
@@ -0,0 +1,14 @@
+#/usr/local/Cellar/python3/3.6.2
+
+def printMsg(msg):
+    print(msg)
+
+if __name__ == '__main__':
+   # print("this is my project")
+    myMsg = "this my project"
+    printMsg(myMsg)
+
+    myMsg = "this is my msg2"
+    printMsg(myMsg)
+
+    printMsg("hello world")
